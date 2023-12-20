5a25cc22c75a1284c1bb96445af9bd7d32cdab20
pullstacker <admin@tartz.one>
2023-12-20 14:07:57 +0700

    spasi

--- config/env.js
+++ config/env.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
 const env = { 
-    PORT: process.env.PORT,
+    PORT: 3001,//process.env.PORT,
 
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_NAME: process.env.DB_NAME,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: '13.214.196.5',//process.env.DB_HOST,
+    DB_PORT: 3306, //process.env.DB_PORT,
+    DB_NAME: 'digicert',//process.env.DB_NAME,
+    DB_USERNAME: 'root',//process.env.DB_USERNAME,
+    DB_PASSWORD: 'Jesus15600D!Yesusb41k!',//process.env.DB_PASSWORD,
     
     PREFIX_SIGNATURE_DATA: process.env.PREFIX_SIGNATURE_DATA,
     JWT_SECRET: process.env.JWT_SECRET,
@@ -16,7 +16,7 @@ const env = {
     FILE_STORAGE: process.env.FILE_STORAGE,
     FILE_MAXIMUM_SIZE: process.env.FILE_MAXIMUM_SIZE,
 
-    ETHER_CLIENT: process.env.ETHER_CLIENT,
+    ETHER_CLIENT: 'https://ethereum-goerli.publicnode.com',//process.env.ETHER_CLIENT,
 
     MAILER_ADDRESS: process.env.MAILER_ADDRESS,
     MAILER_PASSWORD: process.env.MAILER_PASSWORD,
